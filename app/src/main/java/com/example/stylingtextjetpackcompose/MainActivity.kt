44c812aa82c883446c2e18370e568b48660c4760
Momin Tahir <momintahir7@gmail.com>
2022-01-08 18:43:14 +0500

    styling the text

--- app/src/main/java/com/example/stylingtextjetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/stylingtextjetpackcompose/MainActivity.kt
@@ -3,22 +3,37 @@ package com.example.stylingtextjetpackcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.example.stylingtextjetpackcompose.ui.theme.StylingTextJetPackComposeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            StylingTextJetPackComposeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(color = Color(0xFF101010))
+            ) {
+                Text(
+                    text = "JetPack Compose", color = Color.Blue,
+                    fontSize = 30.sp,
+                    textDecoration = TextDecoration.Underline,
+                    textAlign = TextAlign.Center,
+                )
+
             }
         }
     }
